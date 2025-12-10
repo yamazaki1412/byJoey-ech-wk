d87e1258f9263d83730e5fc5c710d50da84bdd60
byJoey <36790372+byJoey@users.noreply.github.com>
2025-12-10 17:45:12 +0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -64,7 +64,7 @@ except ImportError:
     print("安装命令: pip3 install PyQt5")
     sys.exit(1)
 
-APP_VERSION = "1.3"
+APP_VERSION = "1.4"
 APP_TITLE = f"ECH Workers 客户端 v{APP_VERSION}"
 
 # 中国IP列表文件名（离线版本，放在程序目录）
